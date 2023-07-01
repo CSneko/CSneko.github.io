97a036071bf65b7b101ca1e9ebfd1f0a06e8132d
NekoCraft <minecrafthetun@outlook.com>
2023-07-01 18:54:52 +0800

    create 2307010654

--- desktop/js/index.js
+++ desktop/js/index.js
@@ -1,6 +1,6 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "125px";
-  document.getElementById("main").style.marginLeft = "125px";
+  document.getElementById("mySidebar").style.width = "150px";
+  document.getElementById("main").style.marginLeft = "150px";
 }
 
 function closeNav() {
