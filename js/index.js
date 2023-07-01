3e5cc63dd1f8febb3671424d56a71a612720e28f
NekoCraft <minecrafthetun@outlook.com>
2023-07-01 14:50:04 +0800

    test 侧边栏

--- js/index.js
+++ js/index.js
@@ -8,3 +8,14 @@ window.onload = function() {
     htmlElement.classList.add('desktop-style');
   }
 };
+/* 设置侧边栏的宽度为250px，页面内容的左边距为250px */
+function openNav() {
+  document.getElementById("c侧边栏").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* 设置侧边栏宽度为0，页面内容左边距为0 */
+function closeNav() {
+  document.getElementById("c侧边栏").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
