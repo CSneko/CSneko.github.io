973d8d224023e26324e0280fc69f49a968f5548e
NekoCraft <minecrafthetun@outlook.com>
2023-07-01 10:50:25 +0800

    create 2307011050

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
-UA();
+/*
+window.addEventListener('DOMContentLoaded',(event) => {
+	UA();
+});
 //定义函数:获取浏览器使用设备
 function UA()
 {
 //获取窗口宽度
-var wwidth = window.screen.width;
-if (wwidth < 768){
-	document.body.classList.add('mobile-style');
+	var wwidth = window.innerWidth || document.documentElement.clientWidth;
+	if (wwidth < 768){
+		document.getElementById("title").classList.add('mobile-style');
+	}
+	else{
+		document.getElementById("title").classList.add('desktop-style');
+	}
 }
-else{
-	document.body.classList.add('desktop-style');
-}
+*/
+window.onload = function() {
+  var userAgent = navigator.userAgent.toLowerCase();
+  var htmlElement = document.getElementsByTagName('html')[0];
+  
+  if (userAgent.indexOf('mobile') !== -1) {
+    htmlElement.classList.add('mobile-style');
+  } else {
+    htmlElement.classList.add('desktop-style');
+  }
+};
