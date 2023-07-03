e7899f1eaa75ff4202c4d05061c1eee06c45f37a
NekoCraft <minecrafthetun@outlook.com>
2023-07-03 14:07:04 +0800

    update

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+window.onload = function() {
+  var userAgent = navigator.userAgent.toLowerCase();
+  var htmlElement = document.getElementsByTagName('html')[0];
+  
+  if (/mobile|android|iphone|ipad|webos|blackberry|iemobile|opera mini/i.test(userAgent)) {
+    htmlElement.classList.add('mobile-style');
+  } else {
+    htmlElement.classList.add('desktop-style');
+  }
+};
